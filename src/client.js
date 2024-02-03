60d8544364f3a303c4c4d76ef2a1333561aca740
Rich4rd16 <rich4rd16@gmail.com>
2024-02-02 22:43:10 -0500

    first commit

--- src/client.js
+++ src/client.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://eipqkntqjihmjdrbfquw.supabase.co'
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVpcHFrbnRxamlobWpkcmJmcXV3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDY5MTA4NjksImV4cCI6MjAyMjQ4Njg2OX0.ly2bcszn2IGSWY7atZ9s7B_m3K4CoCg2uIXbKGIBUqg'
+const supabaseUrl = '<Ingresar project URL>'
+const supabaseKey = 'API Key'
 export const supabase = createClient(supabaseUrl, supabaseKey)
